7d7b7e4b42acd7f674b897cec92d22b5fa016860
yasser aboelgheit <yasser.aboelgheit@gmail.com>
2025-02-03 10:14:44 +0400

    feat(logs): add logging

--- auth-service-be/src/app.module.ts
+++ auth-service-be/src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { LoggingModule } from './logging/logging.module';
+
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost/auth-app'),
     AuthModule,
+    LoggingModule,
   ],
 })
 export class AppModule {}
